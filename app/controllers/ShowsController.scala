c25f1a25d0168b58535197d65d14fdd112cb9f07
Josef Dolezal <pepik.ml@gmail.com>
2018-06-04 13:39:30 +0200

    Fix initial database evolution

--- app/controllers/ShowsController.scala
+++ app/controllers/ShowsController.scala
@@ -1,13 +1,17 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import models.repository.ShowsRepository
 import play.api.mvc.{AbstractController, ControllerComponents}
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class ShowsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ShowsController @Inject()(cc: ControllerComponents, showsRepo: ShowsRepository)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def shows = Action {
-    Ok(views.html.shows.shows())
+  def shows = Action.async {
+    showsRepo.all.map { shows =>
+      Ok(views.html.shows.shows(shows))
+    }
   }
 
   def show(id: Int) = Action {
--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
 scalaVersion := "2.12.2"
 
 libraryDependencies ++= Seq(
-  ehcache , ws , specs2 % Test , guice,
+  specs2 % Test , guice,
   "com.typesafe.play" %% "play-slick" % "3.0.3",
   "com.typesafe.play" %% "play-slick-evolutions" % "3.0.3",
   "com.h2database" % "h2" % "1.4.197"
