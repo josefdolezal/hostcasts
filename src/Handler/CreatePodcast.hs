25c0753785c76e0d5d6ef56e8cd0a98a46af35a8
Josef Dolezal <pepik.ml@gmail.com>
2018-06-12 21:36:59 +0200

    Add missing podcast model fields

--- src/Handler/CreatePodcast.hs
+++ src/Handler/CreatePodcast.hs
@@ -10,21 +10,29 @@ import Import
 import Yesod.Form.Bootstrap3
 import qualified Forms.PodcastsForms as PF
 
-createPodcastForm :: AForm Handler Podcast
-createPodcastForm = Podcast
+createPodcastForm :: UserId -> AForm Handler Podcast
+createPodcastForm userId = Podcast
     <$> areq textField (attrs "Title") Nothing
     <*> areq textareaField (attrs "Description") Nothing
+    <*> areq (selectFieldList [("English" :: Text, "en-us")]) "Language" Nothing
+    <*> aopt textField (attrs "Tags") Nothing
+    <*> aopt textField (attrs "Copyright notice") Nothing
+    <*> areq checkBoxField (attrs "Is Explicite") Nothing
     <*> aopt textField (attrs "URL") Nothing
+    <*> aopt textField (attrs "Cover") Nothing
+    <*> pure userId
     where attrs placeholder = withPlaceholder placeholder (bfs ("" :: Text))
 
 getCreatePodcastR :: Handler Html
 getCreatePodcastR = do
-    (widget, _) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm createPodcastForm
+    userId <- requireAuthId
+    (widget, _) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm (createPodcastForm userId)
     defaultLayout $(widgetFile "podcasts/new")
 
 postCreatePodcastR :: Handler Html
 postCreatePodcastR = do
-    ((result, widget), _) <- runFormPost $ renderBootstrap3 BootstrapBasicForm createPodcastForm
+    userId <- requireAuthId
+    ((result, widget), _) <- runFormPost $ renderBootstrap3 BootstrapBasicForm (createPodcastForm userId)
     case result of
         FormSuccess podcast -> do
             podcastId <- runDB $ insert podcast
