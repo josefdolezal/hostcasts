079be9f65374b4e3ea7364f2ef0fb010d8a74bd3
Josef Dolezal <pepik.ml@gmail.com>
2018-07-14 09:21:11 +0200

    Fix create action

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -10,7 +10,7 @@ final class TodoController {
     /// Saves a decoded `Todo` to the database.
     func create(_ req: Request) throws -> Future<Todo> {
         return try req.content.decode(Todo.self).flatMap { todo in
-            return todo.save(on: req)
+            return todo.create(on: req)
         }
     }
 
